5fde4d86eb2ea4f57a1dcc09d69f37e3943ebcdf
RubenJuarez <91731294+rubences@users.noreply.github.com>
2022-03-11 17:54:47 +0000

    Initial commit

---
+++ productos/models.py
@@ -0,0 +1,17 @@
+import db
+from sqlalchemy import Column, Integer, String, Float
+class Producto(db.Base):
+    __tablename__ = 'producto'
+    id = Column(Integer, primary_key=True)
+    VALOR = Column(Integer, nullable=False)
+    MEDIA = Column(Float)
+    DESVIACION = Column(Float)
+    DESVIACION_AL_CUADRADO = Column(Float)
+  
+    def __init__(self, VALOR, MEDIA, DESVIACION, DESVIACION_AL_CUADRADO):
+        self.VALOR = VALOR
+        self.MEDIA = MEDIA
+        self.DESVIACION = DESVIACION
+        self.DESVIACION_AL_CUADRADO = DESVIACION_AL_CUADRADO
+    def __repr__(self):
+        return f'Producto({self.VALOR}, {self.MEDIA}, {self.DESVIACION}, {self.DESVIACION_AL_CUADRADO})'
